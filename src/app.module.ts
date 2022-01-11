83972b5108f2c16227d51c9d73331c61de5552fb
S2Onion <ksskss665@naver.com>
2022-01-11 21:23:50 +0900

    Movie 관련 컨트롤러 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 // @Module : Nest가 애플리케이션 구조를 구성하는데 사용하는 메타데이터를 제공
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [MoviesController],
   providers: [],
 })
 export class AppModule {}
