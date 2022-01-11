6cfcc0257fc4df08f103a62d5c5a3e8cc21caef0
S2Onion <ksskss665@naver.com>
2022-01-11 20:45:06 +0900

    컨트롤러 내 URL 추가 연습

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// @Module : Nest가 애플리케이션 구조를 구성하는데 사용하는 메타데이터를 제공
 @Module({
   imports: [],
   controllers: [AppController],
