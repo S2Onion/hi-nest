8dada5716afd15f348ecafe84d58cbb4eca33d03
S2Onion <ksskss665@naver.com>
2022-01-12 21:03:56 +0900

    서비스 개발

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // @Module : Nest가 애플리케이션 구조를 구성하는데 사용하는 메타데이터를 제공
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly movieSearvice: MoviesService){}
+
     @Get()
-    getAll(){
-        return 'this will return all movies';
+    getAll(): Movie[] {
+        return this.movieSearvice.getAll();
     }
 
     @Get("/search")
@@ -14,19 +18,19 @@ export class MoviesController {
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId: string) {
-        return `this will return one movie the movieId: ${movieId}`;
+    getOne(@Param('id') movieId: string): Movie {
+        return this.movieSearvice.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
         console.log(movieData);
-        return movieData;
+        return this.movieSearvice.create(movieData);
     }
 
     @Delete('/:id')
     remove(@Param('id') movieId: string) {
-        return `This will delete a movie with the id: ${movieId}`;
+        return this.movieSearvice.deleteOne(movieId);
     }
 
     @Patch('/:id')
