5e995f9a714d2749c4b460d65b72405f4c5f6237
S2Onion <ksskss665@naver.com>
2022-01-13 20:42:41 +0900

    1. DTO 생성 2. ValidationPipe 설정

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -13,28 +14,28 @@ export class MoviesController {
     }
 
     @Get("/search")
-    search(@Query('year') searchingYear: string) {
+    search(@Query('year') searchingYear: number) {
         return `We are seraching for a movie made after: ${searchingYear}`;
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId: string): Movie {
+    getOne(@Param('id') movieId: number): Movie {
         return this.movieSearvice.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         console.log(movieData);
         return this.movieSearvice.create(movieData);
     }
 
     @Delete('/:id')
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.movieSearvice.deleteOne(movieId);
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId: string, @Body() updateData) {
+    patch(@Param('id') movieId: number, @Body() updateData) {
         this.movieSearvice.update(movieId, updateData);
     }
 
